2423b02552d8f3e748ef89f862aafa2759d282fd
salemk <kammoun.salem@gmail.com>
2015-10-30 12:50:31 +0100

    first commit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,7 +22,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
             new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
 
