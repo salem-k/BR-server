4f3d4d266630f93579dfb96e31655be500237e7a
salemk <kammoun.salem@gmail.com>
2015-10-30 12:33:17 +0100

    first commit

---
+++ src/AppBundle/Controller/RegistrationController.php
@@ -0,0 +1,35 @@
+<?php
+namespace UserBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use UserBundle\Entity\Device;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/create")
+     */
+    public function createAction(Request $request)
+    {
+      $entity = new Device();
+      $lastName =  $request->query->get('lastName');
+      $firstName = $request->request->get('firstName');
+      $email = $request->request->get('email');
+      $deviceToken = $request->request->get('deviceToken');
+      if(!empty($deviceToken) && !empty($email) ){
+        $device = New Device();
+        $device.setLastName($lastName);
+        $device.setFirstName($firstName);
+        $device.setEmail($email);
+        $device.setDeviceToken($deviceToken);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($device);
+        $em->flush();
+        return new JsonResponse(array('registerResponse' => "ok"));
+      }else {
+        return new JsonResponse(array('registerResponse' => "nok"));
+      }
+    }
+}
---
+++ src/AppBundle/Entity/Device.php
@@ -0,0 +1,137 @@
+<?php
+namespace UserBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * Device
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Device
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     */
+    private $lastName;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=255)
+     */
+    private $firstName;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="deviceToken", type="string", length=255)
+     */
+    private $deviceToken;
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return Device
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Device
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+    /**
+     * Set deviceToken
+     *
+     * @param string $deviceToken
+     *
+     * @return Device
+     */
+    public function setDeviceToken($deviceToken)
+    {
+        $this->deviceToken = $deviceToken;
+        return $this;
+    }
+    /**
+     * Get deviceToken
+     *
+     * @return string
+     */
+    public function getDeviceToken()
+    {
+        return $this->deviceToken;
+    }
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Device
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
