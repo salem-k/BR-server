cdce43e04d9b71a1ee318c0e22621609777c818d
salemk <kammoun.salem@gmail.com>
2015-11-03 09:39:31 +0100

    format message

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -78,7 +78,7 @@ class IndexController extends Controller
 
           $cmds = array();
           foreach ($events as $key => $value) {
-            $cmds[$key] = '  curl -u 485d490dd0720a823c518fb6d39d73623ddff1f0487764a4: -H "Content-Type: application/json" -H "X-Ionic-Application-Id: 9cea62b6" https://push.ionic.io/api/v1/push -d \'{"tokens": ["'.$devicesTokens.'"],"production": false, "notification":{  "alert":"'.$events[$key]->__toString().'", "title": "SAMPLE TITLE","android": {"payload":""}, "ios": {"payload": ""}}}\' ';
+            $cmds[$key] = '  curl -u 485d490dd0720a823c518fb6d39d73623ddff1f0487764a4: -H "Content-Type: application/json" -H "X-Ionic-Application-Id: 9cea62b6" https://push.ionic.io/api/v1/push -d \'{"tokens": ["'.$devicesTokens.'"],"production": false, "notification":{  "alert":"'.$events[$key]->__toString().'", "title": "operation","android": {"payload":""}, "ios": {"payload": ""}}}\' ';
             exec($cmds[$key] );
           }
 
